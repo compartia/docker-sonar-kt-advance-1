0157aa1620957424dbe54ae8d769af683b07fc6c
Artem Zaborskiy <compartia@gmail.com>
2018-03-10 13:53:09 +0800

    Update run-demo.sh

--- run-demo.sh
+++ run-demo.sh
@@ -1,5 +1,13 @@
+VER=$(curl -s https://api.github.com/repos/kestreltechnology/sonar-kt-advance/releases/latest | grep tag_name | cut -d '"' -f 4) 
+alias errcho='>&2 echo'
+if [ -z "$VER" ]; then    
+    errcho "ERROR: cannot get version number from GitHub, please check https://api.github.com/repos/kestreltechnology/sonar-kt-advance/releases/latest"
+    exit -1
+fi
+
 export NS=kestreltechnology
-export TAG=5.6.5
+export TAG=$VER
+
 cd ./composite-postgresql-populated-sq
 docker-compose config
 docker-compose up
